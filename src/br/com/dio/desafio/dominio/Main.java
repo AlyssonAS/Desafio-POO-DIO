995a8f536481cb357e84fdffdadc12ad36eb6fe2
Alysson Santiago <allysson9662@gmail.com>
2022-08-04 00:05:13 -0300

    COmmit final

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -14,6 +14,8 @@ public class Main {
         Curso2.setDescricao("Seja muito bem vindo ao Curso Desenvolvimento Web Moderno com JavaScript! COMPLETO 2020 + Projetos! Esse que talvez seja o maior e mais completo curso de tecnologia do mercado. São quase 600 aulas divididas em 33 capítulos com mais de 85 horas.");
         Curso2.setCargaHoraria(96);
 
+        //Conteudo conteudo1 = new curso(); //Polimorfismo
+        //Conteudo conteudo2 = new mentoria()
 
         mentoria Mentoria1 = new mentoria();
         Mentoria1.setTitulo("Spring Framework Experience");
@@ -21,8 +23,48 @@ public class Main {
         Mentoria1.setData(LocalDate.now());
 
 
-        System.out.println(Curso1);
-        System.out.println(Curso2);
-        System.out.println(Mentoria1);
+//        System.out.println(Curso1);
+//        System.out.println(Curso2);
+//        System.out.println(Mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(Curso1);
+        bootcamp.getConteudos().add(Curso2);
+        bootcamp.getConteudos().add(Mentoria1);
+
+        Dev devAlysson = new Dev();
+        devAlysson.setNome("Alysson Sanitago");
+
+        devAlysson.inscreverBootcamp(bootcamp);
+                System.out.println("Alysson - Conteúdo Inscrito:" + devAlysson.getConteudoInscritos());
+
+        System.out.println("-");
+
+        devAlysson.progredir();
+        devAlysson.progredir();
+        devAlysson.progredir();
+                System.out.println("Alysson - Conteúdo Inscrito:" + devAlysson.getConteudoInscritos());
+                System.out.println("Alysson - Conteúdo Concluido:" + devAlysson.getConteudoConcluidos());
+                System.out.println("XP:" + devAlysson.calcularTotalXP());
+
+        System.out.println("_________");
+
+        Dev devPriscilla = new Dev();
+        devPriscilla.setNome("Priscilla Bandeira");
+
+        devPriscilla.inscreverBootcamp(bootcamp);
+                System.out.println("Priscilla - Conteúdo Inscrito:" + devPriscilla.getConteudoInscritos());
+
+        System.out.println("-");
+
+        devPriscilla.progredir();
+        devPriscilla.progredir();
+                System.out.println("Priscilla - Conteúdo Inscrito:" + devPriscilla.getConteudoInscritos());
+                System.out.println("Priscilla - Conteúdo Concluido:" + devPriscilla.getConteudoConcluidos());
+                System.out.println("XP:" + devAlysson.calcularTotalXP());
+
+
     }
 }
